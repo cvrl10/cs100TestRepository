a7d8998407569ffedf9619a43dab1ebca814b51e
Carl Archemetre <cvrlix@gmail.com>
2021-09-28 02:01:05 -0400

    created AddTwoNumbers.java in Linux

---
+++ AddTwoNumbers.java
@@ -0,0 +1,17 @@
+public class AddTwoNumbers
+{
+	public static void main(String[] args)
+	{
+		int sum = 0;
+		int[] num = {12, 24};
+		
+		for (int number : num) //for each loop allows me iterate every element
+			sum += number;
+		
+		System.out.println(sum);
+
+
+	}
+
+
+}
