3098d2610222a82682c2113fdd3813728985da51
Carl Archemetre <cvrlix@gmail.com>
2021-09-28 01:18:07 -0400

    made some changed

--- Application.java
+++ Application.java
@@ -1,13 +1,15 @@
 public class Application
 {
-	{
+
 	public static void main(String[] args)
+	{
 	String[] name = {"Carl","Planel","Archemetre"};
 
 
 
 	for (String str : name)
-		System.out.print(str+" ")
+		System.out.print(str+" ");
+	System.out.println();
 
 
 	}
